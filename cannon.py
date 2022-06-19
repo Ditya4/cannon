0dcfcb76d96159e32fe8ecd9fdfda530f6a6d663
Ditya4 <72632156+Ditya4@users.noreply.github.com>
2022-06-19 13:06:21 +0300

    add targets module, targets, move, hitback walls

--- cannon.py
+++ cannon.py
@@ -4,17 +4,25 @@ from math import acos, pi
 
 class Cannon:
 
-    def __init__(self, x, y, width, height, color, surface):
+    def __init__(self, x, y, width, height, red, surface):
+        """ in case of refactoring wannity:
+            KISS cannon could be a line with width. lol
+        """
         self.x = x
         self.y = y
         self.width = width
         self.height = height
-        self.color = color
+        self.default_height = height
+        self.red = red
+        self.color = (red, 70, 70)
+        self.default_color = (red, 70, 70)
         self.surface = surface
 
     def calculate_angle(self):
         """ first vector = {self.x, self.y; self.x + 50, self.y}
             second vector = {self.x, self.y; mouse_x, mouse_y}
+            finding angle between Ox axis and vector from point (0, 0)
+            to mouse position
         """
         mouse_x, mouse_y = pygame.mouse.get_pos()
         print(mouse_x, mouse_y, " ", end="")
@@ -26,21 +34,15 @@ class Cannon:
         first_point_second_vector_y = self.y
         second_point_second_vextor_x = mouse_x
         second_point_second_vextor_y = mouse_y
-        angle = find_angle(first_point_first_vector_x,
-                           first_point_first_vector_y,
-                           second_point_first_vector_x,
-                           second_point_first_vector_y,
-                           first_point_second_vector_x,
-                           first_point_second_vector_y,
-                           second_point_second_vextor_x,
-                           second_point_second_vextor_y)
+        angle = _find_angle(first_point_first_vector_x,
+                            first_point_first_vector_y,
+                            second_point_first_vector_x,
+                            second_point_first_vector_y,
+                            first_point_second_vector_x,
+                            first_point_second_vector_y,
+                            second_point_second_vextor_x,
+                            second_point_second_vextor_y)
         return angle
-
-    def power_up(self):
-        pass
-
-    def shoot(self):
-        pass
 
     def draw_cannon(self):
         rect = (self.x, self.y, self.width, self.height)
@@ -49,6 +51,7 @@ class Cannon:
         return angle
 
     def draw_rect_angle(self, angle, rect):
+        print(self.color, self.red)
         target_rect = pygame.Rect(rect)
         shape_surf = pygame.Surface(target_rect.size, pygame.SRCALPHA)
         pygame.draw.rect(shape_surf, self.color, (0, 0, *target_rect.size))
@@ -57,8 +60,8 @@ class Cannon:
                           rotated_surf.get_rect(center=target_rect.center))
 
 
-def find_angle(first_a_x, first_a_y, first_b_x, first_b_y,
-               second_a_x, second_a_y, second_b_x, second_b_y):
+def _find_angle(first_a_x, first_a_y, first_b_x, first_b_y,
+                second_a_x, second_a_y, second_b_x, second_b_y):
     """ coords of two vectors
         we steal calculating method from:
         https://ua.onlinemschool.com/math/assistance/vector/angl/
@@ -75,23 +78,12 @@ def find_angle(first_a_x, first_a_y, first_b_x, first_b_y,
     first_length = (first_ab_x ** 2 + first_ab_y ** 2) ** 0.5
     second_length = (second_ab_x ** 2 + second_ab_y ** 2) ** 0.5
     # finding cos(angle)
-    cos_alpha = scalar_product / (first_length * second_length)
-    alpha_in_radian= acos(cos_alpha)
+    if first_length == 0 and second_length == 0:
+        cos_alpha = scalar_product / 0.001
+    else:
+        cos_alpha = scalar_product / (first_length * second_length)
+    alpha_in_radian = acos(cos_alpha)
     alpha = alpha_in_radian * (180 / pi)
-    #convert
+    # convert
     alpha = 90 - alpha
     return alpha
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- cannon_main.py
+++ cannon_main.py
@@ -1,30 +1,77 @@
 import pygame
+from random import randint
 import cannon as cannon_module
+import targets as targets_module
 
 
 def main():
     """ Cannon_x and cannon_y can't be 0 at one time. Error 200. Divising by Zero)
+        red is red component in rgb color
     """
-    cannon_x = 40
-    cannon_y = 40
+    cannon_x = 10
+    cannon_y = -10
     cannon_width = 10
     cannon_height = 40
-    cannon_color = "maroon"
+    red = 0
+    default_red = red
+    '''
+               x, y, x_speed, y_speed, radius, color,
+               left_top_border_x, left_top_border_y,
+               right_bottom_border_x, right_bottom_border_y, surface)
+    '''
+    targets = []
+    target_count = 5
+    min_speed = -6
+    max_speed = 6
+    min_radius = 10
+    max_radius = 25
+    color = "yellow"
+    left_top_border_x = 0
+    left_top_border_y = WINDOW_HEIGHT // 2
+    right_bottom_border_x = WINDOW_WIDTH
+    right_bottom_border_y = WINDOW_HEIGHT
     run = True
     clock = pygame.time.Clock()
     pygame.init()
     surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption("Cannon")
+    cannon = cannon_module.Cannon(cannon_x, cannon_y, cannon_width,
+                                  cannon_height, red, surface)
+    for unused_number in range(target_count):
+        radius = randint(min_radius, max_radius)
+        targets.append(targets_module.Target(
+                randint(left_top_border_x + radius,
+                        right_bottom_border_x - radius),
+                randint(left_top_border_y + radius,
+                        right_bottom_border_y - radius),
+                randint(min_speed, max_speed),
+                randint(min_speed, max_speed),
+                radius, color, left_top_border_x, left_top_border_y,
+                right_bottom_border_x, right_bottom_border_y, surface))
+
     while run:
-        clock.tick(30)
+        clock.tick(20)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
         surface.fill((0, 0, 0))
-        cannon = cannon_module.Cannon(cannon_x, cannon_y, cannon_width,
-                                      cannon_height, cannon_color, surface)
-        print(cannon.calculate_angle())
-        angle = cannon.draw_cannon()
+
+        condition = pygame.mouse.get_pressed(3)[0]
+        print(condition)
+        if condition:
+            if red + 10 <= 244:
+                red += 10
+            cannon.color = (red, 70, 70)
+        else:
+            cannon.color = cannon.default_color
+            red = default_red
+        cannon.calculate_angle()
+        unused_angle = cannon.draw_cannon()
+
+        for target in targets:
+            target.move()
+            target.draw()
+
         pygame.display.update()
 
     pygame.quit()
